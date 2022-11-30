e792388a26fc33d5ad3a9a4f90250dcac3a89772
tsed-alex <tsed.alex@yandex.ru>
2022-11-30 01:41:22 +0100

    feat: add implement function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import { existsSync } from "node:fs";
+import { rename as renameAsync } from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFileNamePath = 'src/fs/wrongFilename.txt';
+    const properFileNamePath = 'src/fs/properFilename.md';
+
+    if (!existsSync(wrongFileNamePath) || existsSync(properFileNamePath)) {
+        throw Error('FS operation failed');
+    }
+
+    try {
+        await renameAsync(wrongFileNamePath, properFileNamePath);
+    }
+    catch (err) {
+        console.error(err);
+    }
 };
 
 await rename();
