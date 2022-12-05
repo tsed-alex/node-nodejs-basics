62a5f7fd20624454cb9173f8aaa15a2289eef119
tsed-alex <tsed.alex@yandex.ru>
2022-12-05 19:48:02 +0100

    feat: add wt implementation

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import * as os from 'os';
+import { Worker } from 'node:worker_threads';
+
+const filePath = new URL('worker.js', import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+    const result = [];
+
+    console.log(os.cpus().length);
+    for (let cpuIndex = 0;  cpuIndex < os.cpus().length; cpuIndex++) {
+        const worker = new Worker(filePath, {
+            workerData: {
+                num: 10 + cpuIndex
+            }
+        });
+        worker.once('message', (msg) => {
+            result.push({ status: 'resolved', data: msg })
+            if(result.length === os.cpus().length) {
+                console.log(result);
+            }
+        });
+    }
 };
 
 await performCalculations();
