a2df87883c5ebe64aaefde011c7810319253ac5b
tsed-alex <tsed.alex@yandex.ru>
2022-12-05 01:09:35 +0100

    feat: add implement zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import * as zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = new URL('archive.gz', import.meta.url);
+    const fileDecompressedPath = new URL('fileToCompress.txt', import.meta.url);
+
+    const input = fs.createReadStream(filePath);
+    const output = fs.createWriteStream(fileDecompressedPath);
+    const gzip = zlib.createUnzip();
+
+    input.pipe(gzip).pipe(output);
 };
 
 await decompress();
